e0d83b94b64d031bce1446d18bd2779e34223715
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-05 12:23:42 +0800

    Setup Contracts

---
+++ src/Contracts/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CleaniqueCoders\KongAdminApi;
+
+/**
+ * Interface Plugin
+ *
+ * Represents a plugin interface for interacting with Kong plugins.
+ */
+interface Plugin
+{
+    // Define plugin-specific methods here if needed.
+}
