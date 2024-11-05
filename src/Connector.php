7be85746370885c67f57deea502967def06e1f53
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-05 12:10:02 +0800

    Added phpdoc block

--- src/Connector.php
+++ src/Connector.php
@@ -4,20 +4,45 @@ namespace CleaniqueCoders\KongAdminApi;
 
 use Saloon\Http\Connector as Base;
 
+/**
+ * Class Connector
+ *
+ * This class provides a connector for the Kong Admin API using the provided configuration.
+ */
 class Connector extends Base
 {
-    public function __construct(protected Configuration $configuration) {}
+    /**
+     * Configuration instance for the Kong Admin API connection.
+     */
+    protected Configuration $configuration;
 
+    /**
+     * Constructor to initialize the connector with a configuration.
+     *
+     * @param  Configuration  $configuration  The configuration settings for the Kong Admin API
+     */
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+    }
+
+    /**
+     * Resolve the base URL for the Kong Admin API.
+     *
+     * @return string The base URL from the configuration
+     */
     public function resolveBaseUrl(): string
     {
         return $this->configuration->getUrl();
     }
 
+    /**
+     * Get the default headers for the Kong Admin API requests.
+     *
+     * @return array<string, string> The headers from the configuration
+     */
     protected function defaultHeaders(): array
     {
-        return [
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json',
-        ];
+        return $this->configuration->getHeaders();
     }
 }
