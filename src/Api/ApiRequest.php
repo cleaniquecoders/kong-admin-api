01723d3e6e4238d63407e8acf4451c7958e97fec
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-05 16:48:49 +0800

    Added common operation for CRUD the Admin API

--- src/Api/ApiRequest.php
+++ src/Api/ApiRequest.php
@@ -21,7 +21,17 @@ class ApiRequest extends Request implements HasBody, RequestContract
     /**
      * The endpoint for the API request.
      */
-    private string $endpoint;
+    protected string $endpoint = '';
+
+    /**
+     * The identifier for the API request.
+     */
+    protected string $identifier = '';
+
+    /**
+     * The data for the API request.
+     */
+    protected array $data = [];
 
     /**
      * Sets the HTTP method for the request.
@@ -39,7 +49,7 @@ class ApiRequest extends Request implements HasBody, RequestContract
     /**
      * Sets the endpoint for the API request.
      *
-     * @param  string  $endpoint  The specific endpoint for the API resource
+     * @param  string  $endpoint  The specific endpoint for the API request
      * @return $this
      */
     public function setEndPoint(string $endpoint): self
@@ -60,12 +70,83 @@ class ApiRequest extends Request implements HasBody, RequestContract
     }
 
     /**
+     * Sets the identifier for the API request.
+     *
+     * @param  string  $identifier  The specific identifier for the API request
+     * @return $this
+     */
+    public function setIdentifier(string $identifier): self
+    {
+        $this->identifier = $identifier;
+
+        return $this;
+    }
+
+    /**
+     * Gets the identifier for the API request.
+     *
+     * @return string The identifier for the API request
+     */
+    public function getIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * Set the data for the API request.
+     *
+     * @param  array  $data  Set the data for the API request
+     * @return $this
+     */
+    public function setData(array $data): self
+    {
+        $this->body()->set($data);
+
+        return $this;
+    }
+
+    public function store(array $data): self
+    {
+        return $this
+            ->setMethod(Method::POST)
+            ->setData($data);
+    }
+
+    public function update(string $identifier, array $data): self
+    {
+        return $this
+            ->setMethod(Method::PUT)
+            ->setIdentifier($identifier)
+            ->setData($data);
+    }
+
+    public function delete(string $identifier): self
+    {
+        return $this
+            ->setMethod(Method::DELETE)
+            ->setIdentifier($identifier);
+    }
+
+    public function get(?string $identifier = null): self
+    {
+        $this->setMethod(Method::GET);
+
+        if ($identifier && is_string($identifier) && ! empty($identifier)) {
+            $this->setIdentifier($identifier);
+        }
+
+        return $this;
+    }
+
+    /**
      * Resolves the endpoint for the request.
      *
      * @return string The fully resolved endpoint for the API request
      */
     public function resolveEndpoint(): string
     {
-        return $this->getEndpoint();
+
+        return $this->getEndpoint()
+            .(! empty($this->getIdentifier()) ? '/'.$this->getIdentifier() : '');
     }
 }
